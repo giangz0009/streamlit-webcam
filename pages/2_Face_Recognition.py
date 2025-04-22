0f636d10b7748c1f255e4b6d275dc519a3169479
Giang Do <giangz0009@gmail.com>
2025-04-22 19:01:33 +0700

    .

--- pages/2_Face_Recognition.py
+++ pages/2_Face_Recognition.py
@@ -1,15 +1,15 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+from streamlit_webrtc import webrtc_streamer
 import av
 import cv2 as cv
 import numpy as np
 import joblib
 
-# Load các model nhận diện
+# Load model nhận diện
 svc = joblib.load('svc.pkl')
 mydict = ['BanKiet', 'BanNghia', 'BanNguyen', 'BanThanh', 'SangSang', 'ThayDuc']
 
-# Load detector và recognizer của OpenCV
+# Load detector và recognizer
 detector = cv.FaceDetectorYN.create(
     'face_detection_yunet_2023mar.onnx',
     "",
@@ -20,57 +20,54 @@ detector = cv.FaceDetectorYN.create(
 )
 
 recognizer = cv.FaceRecognizerSF.create(
-    'face_recognition_sface_2021dec.onnx',
-    ""
+    'face_recognition_sface_2021dec.onnx', ""
 )
+
+# Cờ xác định đã set kích thước detector chưa
+input_size_set = False
 
 # Giao diện Streamlit
 st.title("Nhận diện khuôn mặt bằng Streamlit WebRTC")
 
-class FaceRecognitionTransformer(VideoTransformerBase):
-    def __init__(self):
-        self.tm = cv.TickMeter()
-        self.detector = detector
-        self.recognizer = recognizer
-        self.svc = svc
-        self.labels = mydict
-        self.input_size_set = False
+# Hàm callback xử lý frame (thay cho VideoTransformerBase)
+def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
+    global input_size_set
 
-    def visualize(self, input, faces, fps, thickness=2):
-        if faces[1] is not None:
-            for idx, face in enumerate(faces[1]):
-                coords = face[:-1].astype(np.int32)
-                cv.rectangle(input, (coords[0], coords[1]),
-                             (coords[0]+coords[2], coords[1]+coords[3]), (0, 255, 0), thickness)
-                for i in range(5):
-                    x, y = coords[4 + i*2], coords[5 + i*2]
-                    cv.circle(input, (x, y), 2, (255, 0, 0), thickness)
-        cv.putText(input, 'FPS: {:.2f}'.format(fps), (1, 16),
-                   cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    tm = cv.TickMeter()
 
-    def transform(self, frame: av.VideoFrame) -> np.ndarray:
-        img = frame.to_ndarray(format="bgr24")
+    img = frame.to_ndarray(format="bgr24")
 
-        if not self.input_size_set:
-            h, w, _ = img.shape
-            self.detector.setInputSize([w, h])
-            self.input_size_set = True
+    if not input_size_set:
+        h, w, _ = img.shape
+        detector.setInputSize([w, h])
+        input_size_set = True
 
-        self.tm.start()
-        faces = self.detector.detect(img)
-        self.tm.stop()
+    tm.start()
+    faces = detector.detect(img)
+    tm.stop()
 
-        if faces[1] is not None:
-            face_align = self.recognizer.alignCrop(img, faces[1][0])
-            face_feature = self.recognizer.feature(face_align)
-            prediction = self.svc.predict(face_feature)
-            result = self.labels[prediction[0]]
-            cv.putText(img, result, (1, 50), cv.FONT_HERSHEY_SIMPLEX,
-                       0.5, (0, 255, 0), 2)
+    if faces[1] is not None:
+        face_align = recognizer.alignCrop(img, faces[1][0])
+        face_feature = recognizer.feature(face_align)
+        prediction = svc.predict(face_feature)
+        result = mydict[prediction[0]]
+        cv.putText(img, result, (1, 50), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        self.visualize(img, faces, self.tm.getFPS())
-        return img
+        for idx, face in enumerate(faces[1]):
+            coords = face[:-1].astype(np.int32)
+            cv.rectangle(img, (coords[0], coords[1]), (coords[0]+coords[2], coords[1]+coords[3]), (0, 255, 0), 2)
+            for i in range(5):
+                x, y = coords[4 + i*2], coords[5 + i*2]
+                cv.circle(img, (x, y), 2, (255, 0, 0), 2)
 
-webrtc_streamer(key="face-detection", video_transformer_factory=FaceRecognitionTransformer,rtc_configuration={
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    })
+    fps_text = 'FPS: {:.2f}'.format(tm.getFPS())
+    cv.putText(img, fps_text, (1, 16), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+# Tạo webrtc stream
+webrtc_streamer(
+    key="face-detection",
+    video_frame_callback=video_frame_callback,
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
